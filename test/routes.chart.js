8a35448caa81fa97696683ec88a9f4506d6f3809
brojor <bronislav.jordan@gmail.com>
2022-01-20 11:49:27 +0100

    test: add more chart routes tests

--- test/routes.chart.js
+++ test/routes.chart.js
@@ -72,6 +72,46 @@ describe('GET chart/store-filter-options', function () {
   });
 });
 
+describe('GET chart/aggregated/all', function () {
+  // before(loginUser({ username: 'loumir', password: 'heslo' }, auth));
+  it('It should fail if the user role is regionalManager', async function () {
+    const response = await request(app)
+      .get('/chart/aggregated/all')
+      .set('Authorization', 'Bearer ' + auth.loumir)
+      .expect(401);
+    expect(response.body).to.have.all.keys('success', 'message');
+    expect(response.body.success).to.be.false;
+    expect(response.body.message).to.equal(
+      'Not authorized to access this route'
+    );
+  });
+  it('It should fail if the user role is storeManager', async function () {
+    const response = await request(app)
+      .get('/chart/aggregated/all')
+      .set('Authorization', 'Bearer ' + auth.zahmon)
+      .expect(401);
+    expect(response.body).to.have.all.keys('success', 'message');
+    expect(response.body.success).to.be.false;
+    expect(response.body.message).to.equal(
+      'Not authorized to access this route'
+    );
+  });
+  it('It should respond with json data when user role is topManagement', async function () {
+    const response = await request(app)
+      .get('/chart/aggregated/all')
+      .set('Authorization', 'Bearer ' + auth.kaspav)
+      .query({
+        after: '2021-09-01T00:00:00.000Z',
+        before: '2022-02-28T23:59:59.999Z',
+        detailLevel: 'categories',
+        sortBy: 'id',
+      })
+      .expect(200);
+    response.body.forEach((item) => {
+      expect(item).to.have.all.keys('id', 'deficiencies', 'label');
+    });
+  });
+});
 
 function loginUser(credentials, auth) {
   return function (done) {
