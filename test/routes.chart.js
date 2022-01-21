23cc65f2a881641bb9a7e3f3a92693de3331038b
brojor <bronislav.jordan@gmail.com>
2022-01-21 09:43:34 +0100

    test: add new chart route tests

--- test/routes.chart.js
+++ test/routes.chart.js
@@ -47,6 +47,16 @@ describe('GET chart/store-filter-options', function () {
 });
 
 describe('GET chart/aggregated/all', function () {
+  it('Should fail due to user not logging in', async function () {
+    const response = await request(app)
+      .get('/chart/aggregated/all')
+      .expect(401);
+    expect(response.body).to.have.all.keys('success', 'message');
+    expect(response.body.success).to.be.false;
+    expect(response.body.message).to.equal(
+      'Not authorized to access this route'
+    );
+  });
   it('It should fail if the user role is regionalManager', async function () {
     const response = await request(app)
       .get('/chart/aggregated/all')
@@ -79,6 +89,93 @@ describe('GET chart/aggregated/all', function () {
         detailLevel: 'categories',
         sortBy: 'id',
       })
+      .expect(200);
+    response.body.forEach((item) => {
+      expect(item).to.have.all.keys('id', 'deficiencies', 'label');
+    });
+  });
+});
+
+describe('GET chart/aggregated/:regionId', function () {
+  it('Should fail due to user not logging in', async function () {
+    const response = await request(app)
+      .get('/chart/aggregated/60c39ce6f48a0b1d51d3922d')
+      .expect(401);
+    expect(response.body).to.have.all.keys('success', 'message');
+    expect(response.body.success).to.be.false;
+    expect(response.body.message).to.equal(
+      'Not authorized to access this route'
+    );
+  });
+  it('should fail when a regional manager requests data about foreign region', async function () {
+    const response = await request(app)
+      .get('/chart/aggregated/60c39ce6f48a0b1d51d39230')
+      .set('Authorization', 'Bearer ' + auth.loumir)
+      .expect(401);
+    expect(response.body).to.have.all.keys('success', 'message');
+    expect(response.body.success).to.be.false;
+    expect(response.body.message).to.equal(
+      'Not authorized to access this route'
+    );
+  });
+  it('should fail when user is storeManager', async function () {
+    const response = await request(app)
+      .get('/chart/aggregated/60c39ce6f48a0b1d51d3922d')
+      .set('Authorization', 'Bearer ' + auth.zahmon)
+      .expect(401);
+    expect(response.body).to.have.all.keys('success', 'message');
+    expect(response.body.success).to.be.false;
+    expect(response.body.message).to.equal(
+      'Not authorized to access this route'
+    );
+  });
+  it('It should respond with json data when a regional manager requests data about his region', async function () {
+    const response = await request(app)
+      .get('/chart/aggregated/60c39ce6f48a0b1d51d3922d')
+      .set('Authorization', 'Bearer ' + auth.loumir)
+      .expect(200);
+    response.body.forEach((item) => {
+      expect(item).to.have.all.keys('id', 'deficiencies', 'label');
+    });
+  });
+});
+
+describe('GET chart/individual/:storeId', function () {
+  it('Mělo by selhat, pokud regionalManager žádá data o cizí prodejně', async function () {
+    const response = await request(app)
+      .get('/chart/individual/R4105')
+      .set('Authorization', 'Bearer ' + auth.loumir)
+      .expect(401);
+    expect(response.body).to.have.all.keys('success', 'message');
+    expect(response.body.success).to.be.false;
+    expect(response.body.message).to.equal(
+      'Not authorized to access this route'
+    );
+  });
+  it('Mělo by selhat, pokud storeManager žádá data o cizí prodejně', async function () {
+    const response = await request(app)
+      .get('/chart/individual/R4216')
+      .set('Authorization', 'Bearer ' + auth.zahmon)
+      .expect(401);
+    expect(response.body).to.have.all.keys('success', 'message');
+    expect(response.body.success).to.be.false;
+    expect(response.body.message).to.equal(
+      'Not authorized to access this route'
+    );
+  });
+  it('Mělo by odpovědět JSON daty, pokud regionalManager žádá data o svojí prodejně', async function () {
+    const response = await request(app)
+      .get('/chart/individual/R4221')
+      .set('Authorization', 'Bearer ' + auth.loumir)
+      .expect(200);
+    response.body.forEach((item) => {
+      expect(item).to.have.all.keys('id', 'deficiencies', 'label');
+    });
+  });
+  it('Mělo by odpovědět JSON daty, pokud storeManager žádá data o svojí prodejně', async function () {
+    const response = await request(app)
+      .get('/chart/individual/R4105')
+      .set('Authorization', 'Bearer ' + auth.zahmon)
       .expect(200);
     response.body.forEach((item) => {
       expect(item).to.have.all.keys('id', 'deficiencies', 'label');
